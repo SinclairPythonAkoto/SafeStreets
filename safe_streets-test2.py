998d62ebd2f380f0766f7c40feb6b160d0e1910b
SinclairPythonAkoto <sinclair.python@gmail.com>
2021-11-03 18:59:29 +0000

    script ready for testing - not all postcodes show, codes gets stuck with some address

--- safe_streets-test2.py
+++ safe_streets-test2.py
@@ -14,7 +14,7 @@ def user_address():
     """
       Function to prompt the users postcode or address
     """
-    postcode input("What is your postcode or street address? ")
+    postcode = input("What is your postcode or street address? ")
     return postcode
 
 # convert postcode to longitude & lattitude
@@ -27,3 +27,32 @@ def convert_postcode(address):
         return app.geocode(address).raw
     except:
         return convert_postcode(address)
+
+
+user_address = user_address()
+address = f"{user_address}, United Kingdom"
+location = convert_postcode(address)
+latitude = location["lat"]
+longitude = location["lon"]
+
+# getting current month
+month = datetime.datetime.now()
+month = month.strftime("%m")
+
+# dynamic crime stats via month
+# connect to Police API
+response = requests.get(
+    f'https://data.police.uk/api/crimes-at-location?date=2020-{month}&lat={latitude}&lng={longitude}'
+)
+
+# convert json months into dictionaries
+crime_stats = response.json()[0:]
+
+# print crime stats
+print("2020 Crime Report of Area: ")
+for stats in range(len(crime_stats)):
+    print(f"Category: {crime_stats[stats]['category']}")
+    print(f"Crime ID: {crime_stats[stats]['id']}")
+    print(f"Location: {crime_stats[stats]['location']['street']['name']}")
+    print(f"Outcome: {crime_stats[stats]['outcome_status']}")
+    print("\n")
